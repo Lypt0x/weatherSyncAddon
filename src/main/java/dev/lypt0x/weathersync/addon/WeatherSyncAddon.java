733d100efefebb83ab89c17b281a167b0c508426
Lypt0x <76274659+lypt0x@users.noreply.github.com>
2021-11-24 21:22:13 +0100

    Initial setup

---
+++ src/main/java/dev/lypt0x/weathersync/addon/WeatherSyncAddon.java
@@ -0,0 +1,25 @@
+package dev.lypt0x.weathersync.addon;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+public final class WeatherSyncAddon extends LabyModAddon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+
+}
